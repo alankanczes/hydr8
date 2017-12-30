57a32f77707f4e93257e097bb9650ec46bdf6e88
Alan Kanczes <alankanczes@gmail.com>
2017-12-29 23:47:06 -0800

    Working with status log

---
+++ hydr8_iOS/Hydr8/Device.swift
@@ -0,0 +1,40 @@
+//
+//  BLEConstants.swift
+//  iOSRemoteConfBLEDemo
+//
+//  Created by Evan Stone on 4/9/16.
+//  Copyright © 2016 Cloud City. All rights reserved.
+//
+
+import Foundation
+
+
+//------------------------------------------------------------------------
+// Information about Texas Instruments SensorTag UUIDs can be found at:
+// http://processors.wiki.ti.com/index.php/SensorTag_User_Guide#Sensors
+//------------------------------------------------------------------------
+// From the TI documentation:
+//  The TI Base 128-bit UUID is: F0000000-0451-4000-B000-000000000000.
+//
+//  All sensor services use 128-bit UUIDs, but for practical reasons only
+//  the 16-bit part is listed in this document.
+//
+//  It is embedded in the 128-bit UUID as shown by example below.
+//
+//          Base 128-bit UUID:  F0000000-0451-4000-B000-000000000000
+//          "0xAA01" maps as:   F000AA01-0451-4000-B000-000000000000
+//                                  ^--^
+//------------------------------------------------------------------------
+
+struct Device {
+    
+    static let SensorTagAdvertisingUUID = "AA10"
+    
+    static let BaseUuid = "E0262760-08C2-11E1-9073-0E8AC72E" // with last XXXX
+    
+    static let HeartRateServiceUUID = "E0262760-08C2-11E1-9073-0E8AC72E180D"
+    static let HeartRateMeasurementUUID = "E0262760-08C2-11E1-9073-0E8AC72E2A37"
+
+    static let HeartRateMeasurementDataIndex = 0;
+}
+
--- hydr8_iOS/Hydr8/ViewController.swift
+++ hydr8_iOS/Hydr8/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, CBCentralManagerDelegate, CBPeripheralDelegate, UITextViewDelegate{
 
     //MARK: Properties
     @IBOutlet weak var activityNameTextField: UITextField!
     @IBOutlet weak var activityNameLabel: UILabel!
+    @IBOutlet weak var galvanicResponseLabel: UILabel!
+    @IBOutlet weak var disconnectButton: UIButton!
+    @IBOutlet weak var statusLog: UITextView!
+    
+    // Core Bluetooth properties
+    var centralManager: CBCentralManager!
+    var hydr8Band: CBPeripheral?
+    var galvanicCharacteristic:CBCharacteristic?
+    var keepScanning = false
+    let hydr8BandName = "MAXREFDES73#"
+
+    // define our scanning interval times
+    let timerPauseInterval:TimeInterval = 10.0
+    let timerScanInterval:TimeInterval = 2.0
+    
+    // UI-related
+    let galvanicResponseLabelFontName = "HelveticaNeue-Thin"
+    let galvanicResponseLabelFontSizeMessage:CGFloat = 56.0
+    let galvanicResponseLabelFontSizeTemp:CGFloat = 81.0
+    var lastGalvanicResponse:Int!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +42,12 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         // Handle the text field’s user input through delegate callbacks.
         activityNameTextField.delegate = self
+        centralManager = CBCentralManager(delegate: self,
+                                          queue: nil)
+        
+        statusLog.delegate = self
+        statusLog.isEditable = false
+        statusLog.text = ">> LOG <<"
     }
 
     //MARK: UITextFieldDelegate
@@ -41,6 +69,329 @@ class ViewController: UIViewController, UITextFieldDelegate {
         activityNameLabel.text = "Set default label text."
     }
     
+    // MARK: - CBCentralManagerDelegate methods
+    
+    // Invoked when the central manager’s state is updated.
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        var showAlert = true
+        var message = ""
+        
+        statusLog.text = statusLog.text + "\r> state updated "
+        
+        switch central.state {
+        case.poweredOff:
+            message = "Bluetooth on this device is currently powered off."
+        case.unsupported:
+            message = "This device does not support Bluetooth Low Energy."
+        case.unauthorized:
+            message = "This app is not authorized to use Bluetooth Low Energy."
+        case.resetting:
+            message = "The BLE Manager is resetting; a state update is pending."
+        case.unknown:
+            message = "The state of the BLE Manager is unknown."
+        case.poweredOn:
+            showAlert = false
+            message = "Bluetooth LE is turned on and ready for communication."
+            
+            print(message)
+            keepScanning = true
+            _ = Timer(timeInterval: timerScanInterval, target: self, selector: #selector(pauseScan), userInfo: nil, repeats: false)
+            
+            // Initiate Scan for Peripherals
+            //Option 1: Scan for all devices
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+            
+            // Option 2: Scan for devices that have the service you're interested in...
+            //let sensorTagAdvertisingUUID = CBUUID(string: Device.SensorTagAdvertisingUUID)
+            //print("Scanning for SensorTag adverstising with UUID: \(sensorTagAdvertisingUUID)")
+            //centralManager.scanForPeripheralsWithServices([sensorTagAdvertisingUUID], options: nil)
+            
+        }
+        
+        if showAlert {
+            let alertController = UIAlertController(title: "Central Manager State", message: message, preferredStyle: UIAlertControllerStyle.alert)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+            alertController.addAction(okAction)
+            self.show(alertController, sender: self)
+        }
+    }
+
+    // MARK: - Bluetooth scanning
+    
+    @objc
+    func pauseScan() {
+        // Scanning uses up battery on phone, so pause the scan process for the designated interval.
+        print("*** PAUSING SCAN...")
+        statusLog.text = statusLog.text + "\r> pausing scan "
+
+        _ = Timer(timeInterval: timerPauseInterval, target: self, selector: #selector(resumeScan), userInfo: nil, repeats: false)
+        centralManager.stopScan()
+        disconnectButton.isEnabled = true
+    }
+    
+    @objc
+    func resumeScan() {
+        if keepScanning {
+            // Start scanning again...
+            print("*** RESUMING SCAN!")
+            statusLog.text = "\r> resuming scan "
+
+            disconnectButton.isEnabled = false
+            galvanicResponseLabel.font = UIFont(name: galvanicResponseLabelFontName, size: galvanicResponseLabelFontSizeMessage)
+            statusLog.text = statusLog.text + "\r> Searching"
+            _ = Timer(timeInterval: timerScanInterval, target: self, selector: #selector(pauseScan), userInfo: nil, repeats: false)
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+        } else {
+            disconnectButton.isEnabled = true
+        }
+    }
+
+    /*
+     Invoked when the central manager discovers a peripheral while scanning.
+     
+     The advertisement data can be accessed through the keys listed in Advertisement Data Retrieval Keys.
+     You must retain a local copy of the peripheral if any command is to be performed on it.
+     In use cases where it makes sense for your app to automatically connect to a peripheral that is
+     located within a certain range, you can use RSSI data to determine the proximity of a discovered
+     peripheral device.
+     
+     central - The central manager providing the update.
+     peripheral - The discovered peripheral.
+     advertisementData - A dictionary containing any advertisement data.
+     RSSI - The current received signal strength indicator (RSSI) of the peripheral, in decibels.
+     
+     */
+    func centralManager(_didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], rssi: NSNumber) {
+        print("centralManager didDiscoverPeripheral - CBAdvertisementDataLocalNameKey is \"\(CBAdvertisementDataLocalNameKey)\"")
+        
+        // Retrieve the peripheral name from the advertisement data using the "kCBAdvDataLocalName" key
+        if let peripheralName = advertisementData[CBAdvertisementDataLocalNameKey] as? String {
+            print("NEXT PERIPHERAL NAME: \(peripheralName)")
+            print("NEXT PERIPHERAL UUID: \(peripheral.identifier.uuidString)")
+            
+            statusLog.text = statusLog.text + "\r> peripheral \(peripheralName) "
+
+            
+            if peripheralName == hydr8BandName {
+                print("HYDR8 BAND FOUND! ADDING NOW!!!")
+                statusLog.text = statusLog.text + "\r> BAND FOUND! "
+                
+                // to save power, stop scanning for other devices
+                keepScanning = false
+                disconnectButton.isEnabled = true
+                
+                // save a reference to the sensor tag
+                hydr8Band = peripheral
+                hydr8Band!.delegate = self
+                
+                // Request a connection to the peripheral
+                centralManager.connect(hydr8Band!, options: nil)
+            }
+        }
+    }
+    
+    
+    /*
+     Invoked when a connection is successfully created with a peripheral.
+     
+     This method is invoked when a call to connectPeripheral:options: is successful.
+     You typically implement this method to set the peripheral’s delegate and to discover its services.
+     */
+    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
+        print("**** SUCCESSFULLY CONNECTED TO BAND!!!")
+        
+        galvanicResponseLabel.font = UIFont(name: galvanicResponseLabelFontName, size: galvanicResponseLabelFontSizeMessage)
+        statusLog.text = statusLog.text + "\r> Connected"
+        
+        // Now that we've successfully connected to the SensorTag, let's discover the services.
+        // - NOTE:  we pass nil here to request ALL services be discovered.
+        //          If there was a subset of services we were interested in, we could pass the UUIDs here.
+        //          Doing so saves battery life and saves time.
+        peripheral.discoverServices(nil)
+    }
+    
+    
+    /*
+     Invoked when the central manager fails to create a connection with a peripheral.
+     
+     This method is invoked when a connection initiated via the connectPeripheral:options: method fails to complete.
+     Because connection attempts do not time out, a failed connection usually indicates a transient issue,
+     in which case you may attempt to connect to the peripheral again.
+     */
+    /*FIXME*/ func centralManager(_didFailToConnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        print("**** CONNECTION TO BAND FAILED!!!")
+        statusLog.text = statusLog.text + "\r> Connection failed"
+
+    }
+    
+    
+    /*
+     Invoked when an existing connection with a peripheral is torn down.
+     
+     This method is invoked when a peripheral connected via the connectPeripheral:options: method is disconnected.
+     If the disconnection was not initiated by cancelPeripheralConnection:, the cause is detailed in error.
+     After this method is called, no more methods are invoked on the peripheral device’s CBPeripheralDelegate object.
+     
+     Note that when a peripheral is disconnected, all of its services, characteristics, and characteristic descriptors are invalidated.
+     */
+    /*FIXME*/ func centralManager(_didDisconnectPeripheral peripheral: CBPeripheral, error: NSError?) {
+        print("**** DISCONNECTED FROM BAND!!!")
+        statusLog.text = statusLog.text + "\r> Disconnected from band"
+
+        // CHANGE ME lastTemperature = 0
+        //updateBackgroundImageForTemperature(lastTemperature)
+        //circleView.hidden = true
+        galvanicResponseLabel.font = UIFont(name: galvanicResponseLabelFontName, size: galvanicResponseLabelFontSizeMessage)
+        galvanicResponseLabel.text = "Tap to search"
+        if error != nil {
+            print("****** DISCONNECTION DETAILS: \(error!.localizedDescription)")
+        }
+        hydr8Band = nil
+    }
+    
+    
+    //MARK: - CBPeripheralDelegate methods
+    
+    /*
+     Invoked when you discover the peripheral’s available services.
+     
+     This method is invoked when your app calls the discoverServices: method.
+     If the services of the peripheral are successfully discovered, you can access them
+     through the peripheral’s services property.
+     
+     If successful, the error parameter is nil.
+     If unsuccessful, the error parameter returns the cause of the failure.
+     */
+    // When the specified services are discovered, the peripheral calls the peripheral:didDiscoverServices: method of its delegate object.
+    /*FIXME*/     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if error != nil {
+            print("ERROR DISCOVERING SERVICES: \(String(describing: error?.localizedDescription))")
+            statusLog.text = statusLog.text + "\r> Discovery failed"
+
+            return
+        }
+        
+        // Core Bluetooth creates an array of CBService objects —- one for each service that is discovered on the peripheral.
+        if let services = peripheral.services {
+            for service in services {
+                print("Discovered service \(service)")
+                statusLog.text = statusLog.text + "\r> Discovered service \(service)"
+
+                // If we found a service, discover the characteristics for those services.
+                if (service.uuid == CBUUID(string: Device.HeartRateServiceUUID)) {
+                    peripheral.discoverCharacteristics(nil, for: service)
+                }
+            }
+        }
+    }
+    
+    
+    /*
+     Invoked when you discover the characteristics of a specified service.
+     
+     If the characteristics of the specified service are successfully discovered, you can access
+     them through the service's characteristics property.
+     
+     If successful, the error parameter is nil.
+     If unsuccessful, the error parameter returns the cause of the failure.
+     */
+    func peripheral(_didDiscoverCharacteristicsFor service: CBService, error: NSError?) {
+        if error != nil {
+            print("ERROR DISCOVERING CHARACTERISTICS: \(String(describing: error?.localizedDescription))")
+            statusLog.text = statusLog.text + "\r> Error discover characteristics"
+
+            return
+        }
+        
+        if let characteristics = service.characteristics {
+            var enableValue:UInt8 = 1
+            // OLD: let enableBytes = NSData(bytes: &enableValue, length: sizeof(UInt8))
+            let enableBytes = NSData(bytes: &enableValue, length: MemoryLayout<UInt8>.size)
+
+            for characteristic in characteristics {
+                // Temperature Data Characteristic
+                if characteristic.uuid == CBUUID(string: Device.HeartRateMeasurementUUID) {
+                    // Enable the notifications
+                    galvanicCharacteristic = characteristic
+                    hydr8Band?.setNotifyValue(true, for: characteristic)
+                }
+                
+/*
+                 // Temperature Configuration Characteristic
+                if characteristic.uuid == CBUUID(string: Device.TemperatureConfig) {
+                    // Enable IR Temperature Sensor
+                    hydr8Band?.writeValue(enableBytes as Data, for: characteristic, type: .withResponse)
+                }
+*/
+                
+            }
+        }
+    }
+    
+    
+    /*
+     Invoked when you retrieve a specified characteristic’s value,
+     or when the peripheral device notifies your app that the characteristic’s value has changed.
+     
+     This method is invoked when your app calls the readValueForCharacteristic: method,
+     or when the peripheral notifies your app that the value of the characteristic for
+     which notifications and indications are enabled has changed.
+     
+     If successful, the error parameter is nil.
+     If unsuccessful, the error parameter returns the cause of the failure.
+     */
+    func peripheral(_didUpdateValueFor characteristic: CBCharacteristic, error: NSError?) {
+        if error != nil {
+            print("ERROR ON UPDATING VALUE FOR CHARACTERISTIC: \(characteristic) - \(String(describing: error?.localizedDescription))")
+            statusLog.text = statusLog.text + "\r> Error updating characteristics"
+
+            return
+        }
+        
+        // extract the data from the characteristic's value property and display the value based on the characteristic type
+        if let dataBytes = characteristic.value {
+            if characteristic.uuid == CBUUID(string: Device.HeartRateMeasurementUUID) {
+                displayGalvanicResponse(data: dataBytes as NSData)
+/* HUMIDITY
+            } else if characteristic.UUID == CBUUID(string: Device.HumidityDataUUID) {
+                displayHumidity(dataBytes)
+ */
+            }
+        }
+    }
+
+    func displayGalvanicResponse(data:NSData) {
+        // We'll get four bytes of data back, so we divide the byte count by two
+        // because we're creating an array that holds two 16-bit (two-byte) values
+        let dataLength = data.length / MemoryLayout<UInt16>.size
+        var dataArray = [UInt16](repeating: 0, count:dataLength)
+        data.getBytes(&dataArray, length: dataLength * MemoryLayout<Int16>.size)
+        
+        //        // output values for debugging/diagnostic purposes
+        //        for i in 0 ..< dataLength {
+        //            let nextInt:UInt16 = dataArray[i]
+        //            print("next int: \(nextInt)")
+        //        }
+        
+        let rawHeartRate:UInt16 = dataArray[Device.HeartRateMeasurementDataIndex]
+
+        lastGalvanicResponse = Int(rawHeartRate)
+        print("*** LAST HEARTRATE CAPTURED: \(lastGalvanicResponse)")
+        statusLog.text = statusLog.text + "\r> *** LAST HEARTRATE CAPTURED: \(lastGalvanicResponse)"
+
+        if UIApplication.shared.applicationState == .active {
+            updateTemperatureDisplay()
+        }
+    }
+
+    // MARK: - Updating UI
+    
+    func updateTemperatureDisplay() {
+
+        galvanicResponseLabel.font = UIFont(name: galvanicResponseLabelFontName, size: galvanicResponseLabelFontSizeTemp)
+        galvanicResponseLabel.text = " \(lastGalvanicResponse)"
+    }
+        
     
 }
 
